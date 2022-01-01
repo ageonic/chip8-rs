3d71bc4367203b21bd4cd77226998eec6ce67cdb
ageonic <angeorge.dev@gmail.com>
2021-12-31 23:30:15 -0500

    Add unit tests for the instruction module

--- src/instruction.rs
+++ src/instruction.rs
@@ -37,3 +37,59 @@ impl Opcode {
         (self.x, self.y, self.n, self.nn, self.nnn)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::instruction::Opcode;
+
+    #[test]
+    fn from_interprets_00e0() {
+        let op = Opcode::from(0x00E0);
+        assert_eq!(0x0, op.category);
+        assert_eq!(0x0, op.x);
+        assert_eq!(0xE, op.y);
+        assert_eq!(0x0, op.n);
+        assert_eq!(0xE0, op.nn);
+        assert_eq!(0x0E0, op.nnn);
+    }
+
+    #[test]
+    fn from_interprets_a2ec() {
+        let op = Opcode::from(0xA2EC);
+        assert_eq!(0xA, op.category);
+        assert_eq!(0x2, op.x);
+        assert_eq!(0xE, op.y);
+        assert_eq!(0xC, op.n);
+        assert_eq!(0xEC, op.nn);
+        assert_eq!(0x2EC, op.nnn);
+    }
+
+    #[test]
+    fn from_interprets_87a1() {
+        let op = Opcode::from(0x87A1);
+        assert_eq!(0x8, op.category);
+        assert_eq!(0x7, op.x);
+        assert_eq!(0xA, op.y);
+        assert_eq!(0x1, op.n);
+        assert_eq!(0xA1, op.nn);
+        assert_eq!(0x7A1, op.nnn);
+    }
+
+    #[test]
+    fn as_u16_returns_raw_opcode() {
+        let op = Opcode::from(0xA32E);
+        assert_eq!(0xA32E, op.as_u16());
+    }
+
+    #[test]
+    fn as_nibbles_returns_expected_tuple() {
+        let op = Opcode::from(0xA32E);
+        assert_eq!((0xA, 0x3, 0x2, 0xE), op.as_nibbles());
+    }
+
+    #[test]
+    fn as_destructured_params_returns_expected_tuple() {
+        let op = Opcode::from(0xA32E);
+        assert_eq!((0x3, 0x2, 0xE, 0x2E, 0x32E), op.as_destructured_params());
+    }
+}
